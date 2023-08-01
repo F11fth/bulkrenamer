05848a462e4123cb102e3caf001465da964b4438
F1fth <mail-github@f1fth.com>
2023-08-01 09:48:21 +0200

    Input komplett auf Argumente umgestellt

--- bulkrenamer.py
+++ bulkrenamer.py
@@ -1,13 +1,19 @@
 import os
+import sys
 import datetime
 
 def main():
 	
 	timestamp = datetime.datetime.now()
 	date = timestamp.date().isoformat()
-	path = "/Users/f1fth/Desktop/fotosort/"
+	#ToDo: Compress
+	
+	path = sys.argv[1]
+	stocktype = sys.argv[2]
+	stocknumber = sys.argv[3]
+	#ToDo: add argparse
 	for filename in os.listdir(path):
-		target = date + "-" + filename
+		target = date + "-" + stocktype + "-" + stocknumber + "-" + filename
 		source = path + filename
 		target = path + target
 		os.rename(source, target)
